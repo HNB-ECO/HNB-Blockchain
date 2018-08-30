376e0e871bce42130ee951670d87001635fbb638
William.zhao <william.zhao@hnb.eco>
2018-08-30 23:37:44 +0800

    add bsscp SoftWare

---
+++ HNB/bccsp/sw/hash.go
@@ -0,0 +1,23 @@
+
+
+package sw
+
+import (
+	"hash"
+
+	"HNB/bccsp"
+)
+
+type hasher struct {
+	hash func() hash.Hash
+}
+
+func (c *hasher) Hash(msg []byte, opts bccsp.HashOpts) (hash []byte, err error) {
+	h := c.hash()
+	h.Write(msg)
+	return h.Sum(nil), nil
+}
+
+func (c *hasher) GetHash(opts bccsp.HashOpts) (h hash.Hash, err error) {
+	return c.hash(), nil
+}
