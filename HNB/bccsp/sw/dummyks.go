16c5b04654852df9f99f82b49247786e559ff510
William.zhao <william.zhao@hnb.eco>
2018-09-12 21:00:49 +0800

    dummy store

---
+++ HNB/bccsp/sw/dummyks.go
@@ -0,0 +1,28 @@
+
+package sw
+
+import (
+	"errors"
+
+	"github.com/HNB-ECO/HNB-Blockchain/HNB/bccsp"
+)
+
+
+func NewDummyKeyStore() bccsp.KeyStore {
+	return &dummyKeyStore{}
+}
+
+type dummyKeyStore struct {
+}
+
+func (ks *dummyKeyStore) ReadOnly() bool {
+	return true
+}
+
+func (ks *dummyKeyStore) GetKey(ski []byte) (k bccsp.Key, err error) {
+	return nil, errors.New("Key not found. This is a dummy KeyStore")
+}
+
+func (ks *dummyKeyStore) StoreKey(k bccsp.Key) (err error) {
+	return errors.New("Cannot store key. This is a dummy read-only KeyStore")
+}
