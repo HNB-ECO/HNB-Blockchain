ee3c4db955cfea98393548e4251b99593f6e3d04
James.wang <James.wang@hnb.eco>
2018-09-16 17:49:09 +0800

    add test

---
+++ HNB/msp/msp_test.go
@@ -0,0 +1,30 @@
+package msp
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestSign(t *testing.T) {
+	priKey, err := GeneratePriKey(1)
+	if err != nil {
+		fmt.Println("err1", err)
+	}
+	NewKeyPair()
+	keyPair.PriKey = priKey
+	pubKey, err := priKey.PublicKey()
+	if err != nil {
+		fmt.Println("err2", err)
+	}
+	keyPair.PubKey = pubKey
+	keyPair.Scheme = 1
+	data, err := Sign([]byte("123"))
+	if err != nil {
+		fmt.Println("err3", err)
+	}
+	fmt.Println(data)
+	keyStr := PubKeyToString(keyPair.Scheme, keyPair.PubKey)
+	keyO := GetBccspKeyFromPubKey(keyStr)
+	ok , err := Verify(keyO, data, []byte("123"))
+	fmt.Println(ok, err)
+}
