be94d87833eb71e03f3cc5bd005990fbfe6726c0
ZornChen <Zorn.chen@hnb.eco>
2018-11-16 16:06:02 +0800

    rest queue

--- HNB/access/rest/queue.go
+++ HNB/access/rest/queue.go
@@ -5,21 +5,52 @@ import (
 	"net/http"
 	"encoding/json"
 	"HNB/txpool"
-	"HNB/common"
+	"HNB/util"
 )
 
 
-type txInfo struct {
-	Queue map[common.Address]common.Transactions
-	Pending map[common.Address]common.Transactions
+type TxsInfo struct {
+	Queue []*TxInfo    `json:"queue"`
+	Pending []*TxInfo  `json:"pending"`
+}
+
+
+type TxInfo struct {
+	Nonce uint64 `json:"nonce"`
+	From string	 `json:"from"`
+	Txid string  `json:"txid"`
 }
 
 func (*serverREST) GetTxPoolQueue(rw web.ResponseWriter, req *web.Request) {
 	rw.WriteHeader(http.StatusOK)
 	encoder := json.NewEncoder(rw)
 	queue, pending := txpool.GetContent()
-	ti := &txInfo{queue, pending}
-	retMsg := FormatQueryResResult("0000", "", ti)
+	var queueInfo,pendingInfo []*TxInfo
+
+
+	for k,v := range queue{
+		for _, tx := range v{
+			ti := &TxInfo{}
+			ti.Txid = util.ByteToHex(tx.Txid.GetBytes())
+			ti.From = util.ByteToHex(k.GetBytes())
+			ti.Nonce = tx.Nonce()
+			queueInfo = append(queueInfo, ti)
+		}
+	}
+
+	for k,v := range pending{
+		for _, tx := range v{
+			ti := &TxInfo{}
+			ti.Txid = util.ByteToHex(tx.Txid.GetBytes())
+			ti.From = util.ByteToHex(k.GetBytes())
+			ti.Nonce = tx.Nonce()
+			pendingInfo = append(pendingInfo, ti)
+		}
+	}
+
+
+	tsi := &TxsInfo{queueInfo, pendingInfo}
+	retMsg := FormatQueryResResult("0000", "", tsi)
 	encoder.Encode(retMsg)
 }
 
