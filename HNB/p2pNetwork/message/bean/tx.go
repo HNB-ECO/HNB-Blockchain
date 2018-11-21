0d3a646389410f9a9873472c65e09e7fb2c55aa0
William.zhao <william.zhao@hnb.eco>
2018-11-21 21:09:24 +0800

    add network tx

---
+++ HNB/p2pNetwork/message/bean/tx.go
@@ -0,0 +1,24 @@
+package bean
+
+import (
+	"HNB/p2pNetwork/common"
+)
+
+type TxMsg struct {
+	Msg []byte
+}
+
+//Serialize message payload
+func (this TxMsg) Serialization() ([]byte, error) {
+	return this.Msg, nil
+}
+
+func (this *TxMsg) CmdType() string {
+	return common.TX_TYPE
+}
+
+//Deserialize message payload
+func (this *TxMsg) Deserialization(p []byte) error {
+	this.Msg = p
+	return nil
+}
