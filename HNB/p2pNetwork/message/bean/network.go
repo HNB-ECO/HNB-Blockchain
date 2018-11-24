fc76a49322ba944e3979ed2c32aa272affb39078
William.zhao <william.zhao@hnb.eco>
2018-11-24 15:05:50 +0800

    add network network

---
+++ HNB/p2pNetwork/message/bean/network.go
@@ -0,0 +1,43 @@
+package bean
+
+import (
+	"HNB/p2pNetwork/common"
+	"encoding/json"
+	"errors"
+)
+
+type TestNetwork struct {
+	Msg string
+}
+
+//Serialize message payload
+func (this TestNetwork) Serialization() ([]byte, error) {
+	//p := bytes.NewBuffer([]byte{})
+	//err := binary.Write(p, binary.LittleEndian, &(this.Msg))
+	//if err != nil {
+	//	return nil, errors.New("123")
+	//}
+	//
+	//return p.Bytes(), nil
+
+	m, err := json.Marshal(this.Msg)
+	if err != nil {
+		return nil, errors.New("123")
+	}
+	return m, nil
+}
+
+func (this *TestNetwork) CmdType() string {
+	return common.TEST_NETWORK
+}
+
+//Deserialize message payload
+func (this *TestNetwork) Deserialization(p []byte) error {
+	var msg string
+	err := json.Unmarshal(p, &msg)
+	if err != nil {
+		return errors.New("456")
+	}
+	this.Msg = msg
+	return nil
+}
