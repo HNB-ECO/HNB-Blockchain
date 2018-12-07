22717b77d029ec94eb7f337f170ff793382a7eb9
ZornChen <Zorn.chen@hnb.eco>
2018-12-07 09:29:15 +0800

    hgs -> hnb

--- HNB/contract/hgs/hgs.go
+++ HNB/contract/hgs/hgs.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 	"HNB/util"
 	"bytes"
+	"HNB/txpool"
 )
 
 var HgsLog logging.LogModule
@@ -18,11 +19,20 @@ const (
 
 type hsgTx struct {
 	OutputAddr []byte `json:"output"`
-	Amount     int64          `json:"amount"`
+	Amount     int64  `json:"amount"`
+	//1  same   2  diff
+	TxType     uint8  `json:"txType"`
+	//only TxType = 2 valid
+	InAmount int64    `json:"inAmount"`
 }
 
 type hgs struct {
 }
+
+const(
+	SAME = 1
+	DIFF = 2
+)
 
 var user = []string{"68175250cadc6f2524f55e891e244116fec4b690"}
 
@@ -49,15 +59,7 @@ func (h *hgs) Init() error {
 	return nil
 }
 
-func (h *hgs) Invoke(ca appComm.ContractApi) error {
-	HgsLog.Debugf(LOGTABLE_HGS, "Invoke args: %v", ca.GetArgs())
-
-	arg := ca.GetArgs()
-	hx := hsgTx{}
-	err := json.Unmarshal([]byte(arg), &hx)
-	if err != nil {
-		return err
-	}
+func (h *hgs)SamePro(ca appComm.ContractApi, hx *hsgTx) error{
 
 	fromAddr := ca.GetFrom()
 	from := fromAddr.GetBytes()
@@ -85,10 +87,67 @@ func (h *hgs) Invoke(ca appComm.ContractApi) error {
 		bAmount, _ = strconv.ParseInt(string(output), 10, 64)
 	}
 
-	a := strconv.FormatInt(inputAmount-hx.Amount, 10)
-	b := strconv.FormatInt(bAmount+hx.Amount, 10)
+	a := strconv.FormatInt(inputAmount - hx.Amount, 10)
+	b := strconv.FormatInt(bAmount + hx.Amount, 10)
 	ca.PutState(from, []byte(a))
 	ca.PutState(hx.OutputAddr, []byte(b))
+
+	return nil
+}
+func (h *hgs)DiffPro(ca appComm.ContractApi, hx *hsgTx) error{
+	fromAddr := ca.GetFrom()
+	from := fromAddr.GetBytes()
+
+	//if bytes.Compare(from, hx.OutputAddr) == 0{
+	//	return errors.New("in and out addr same")
+	//}
+
+	input, _ := ca.GetState(from)
+	if input == nil {
+		return errors.New("input not exist")
+	}
+
+	inputAmount, _ := strconv.ParseInt(string(input), 10, 64)
+
+	if inputAmount < hx.InAmount {
+		return errors.New("insufficient balance")
+	}
+
+	var bAmount int64 = 0
+
+	//from other coin
+	output, _ := ca.GetOtherState(txpool.HNB, hx.OutputAddr)
+	//output, _ := ca.GetState(hx.OutputAddr)
+	if output != nil {
+		bAmount, _ = strconv.ParseInt(string(output), 10, 64)
+	}
+
+	a := strconv.FormatInt(inputAmount - hx.InAmount, 10)
+	b := strconv.FormatInt(bAmount + hx.Amount, 10)
+	ca.PutState(from, []byte(a))
+
+	//from other coin
+	ca.PutOtherState(txpool.HNB, hx.OutputAddr, []byte(b))
+
+	return nil
+}
+
+func (h *hgs) Invoke(ca appComm.ContractApi) error {
+	HgsLog.Debugf(LOGTABLE_HGS, "Invoke args: %v", ca.GetArgs())
+
+	arg := ca.GetArgs()
+	hx := hsgTx{}
+	err := json.Unmarshal([]byte(arg), &hx)
+	if err != nil {
+		return err
+	}
+
+	if hx.TxType == SAME{
+		return h.SamePro(ca, &hx)
+	}else{
+		return h.DiffPro(ca, &hx)
+	}
+
 
 	return nil
 }
