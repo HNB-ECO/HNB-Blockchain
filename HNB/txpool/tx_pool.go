fc30b33a385e41ab8f5d8d51ebbdf83947fd3a79
William.zhao <william.zhao@hnb.eco>
2018-12-27 00:21:57 +0800

    update tx_pool

--- HNB/txpool/tx_pool.go
+++ HNB/txpool/tx_pool.go
@@ -91,3 +91,128 @@ type TxPool struct {
 
 	wg sync.WaitGroup // for shutdown sync
 }
+type RecvTxStruct struct {
+	recvTx []byte
+	local  bool
+}
+
+// NewTxPool creates a new transaction pool to gather, sort and filter inbound
+// transactions from the network.
+func NewTxPool(config TxPoolConfig) *TxPool {
+	// Sanitize the input to ensure no vulnerable gas prices are set
+	config = (&config).sanitize()
+	chainID := new(big.Int)
+	chainID.SetBytes([]byte(common.HGS))
+
+	// Create the transaction pool with its initial settings
+	pool := &TxPool{
+		config:     config,
+		signer:     msp.NewHNBSigner(chainID),
+		pending:    make(map[common.Address]*txList),
+		queue:      make(map[common.Address]*txList),
+		beats:      make(map[common.Address]time.Time),
+		all:        newTxLookup(),
+		recvBlkTxs: make(chan common.Transactions),
+		notify:     make(chan struct{}, 10),
+		recvTx:     make(chan *RecvTxStruct, 10000),
+	}
+	pool.locals = newAccountSet(pool.signer)
+	for _, addr := range config.Locals {
+		TXPoolLog.Infof(LOGTABLE_TXPOOL, "Setting new local account", "address", addr)
+		pool.locals.add(addr)
+	}
+
+	pool.reset(nil)
+
+	// If local transactions and journaling is enabled, load from disk
+	if !config.NoLocals && config.Journal != "" {
+		pool.journal = newTxJournal(config.Journal)
+
+		if err := pool.journal.load(pool.AddLocals); err != nil {
+			TXPoolLog.Warningf(LOGTABLE_TXPOOL, "Failed to load transaction journal", "err", err)
+		}
+		if err := pool.journal.rotate(pool.local()); err != nil {
+			TXPoolLog.Warningf(LOGTABLE_TXPOOL, "Failed to rotate transaction journal", "err", err)
+		}
+	}
+
+	// Start the event loop and return
+	pool.wg.Add(1)
+	go pool.loop()
+
+	go pool.handlerTx()
+	return pool
+}
+
+// loop is the transaction pool's main event loop, waiting for and reacting to
+// outside blockchain events as well as for various reporting and transaction
+// eviction events.
+func (pool *TxPool) loop() {
+	defer pool.wg.Done()
+
+	// Start the stats reporting and transaction eviction tickers
+	var prevPending, prevQueued int
+
+	report := time.NewTicker(statsReportInterval)
+	defer report.Stop()
+
+	evict := time.NewTicker(evictionInterval)
+	defer evict.Stop()
+
+	journal := time.NewTicker(pool.config.Rejournal)
+	defer journal.Stop()
+
+	// Track the previous head headers for transaction reorgs
+
+	// Keep waiting for and reacting to the various events
+	for {
+		select {
+		// Handle ChainHeadEvent
+		case txs := <-pool.recvBlkTxs:
+
+			if txs != nil {
+				pool.mu.Lock()
+				TXPoolLog.Debugf(LOGTABLE_TXPOOL, "del tx length %v", len(txs))
+				pool.reset(txs)
+				pool.mu.Unlock()
+			}
+
+		case <-report.C:
+			pool.mu.RLock()
+			pending, queued := pool.stats()
+			pool.mu.RUnlock()
+
+			if pending != prevPending || queued != prevQueued {
+				TXPoolLog.Debugf(LOGTABLE_TXPOOL, "Transaction pool status report", "executable", pending, "queued", queued)
+				prevPending, prevQueued = pending, queued
+			}
+
+			// Handle inactive account transaction eviction
+		case <-evict.C:
+			pool.mu.Lock()
+			for addr := range pool.queue {
+				// Skip local transactions from the eviction mechanism
+				if pool.locals.contains(addr) {
+					continue
+				}
+				// Any non-locals old enough should be removed
+				if time.Since(pool.beats[addr]) > pool.config.Lifetime {
+					for _, tx := range pool.queue[addr].Flatten() {
+						pool.removeTx(tx.Hash(), true)
+					}
+				}
+			}
+			pool.mu.Unlock()
+
+			// Handle local transaction journal rotation
+		case <-journal.C:
+			if pool.journal != nil {
+				pool.mu.Lock()
+				if err := pool.journal.rotate(pool.local()); err != nil {
+					TXPoolLog.Warningf(LOGTABLE_TXPOOL, "Failed to rotate local tx journal", "err", err)
+				}
+				pool.mu.Unlock()
+			}
+		}
+	}
+}
