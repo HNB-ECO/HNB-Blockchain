f50607f0b9dbca73de9d793eb3c6282aeefa952f
jeremyv1 <2273675070@qq.com>
2018-12-26 12:02:01 +0800

    ecdsa algo update

--- HNB/bccsp/ecdsaopts.go
+++ HNB/bccsp/ecdsaopts.go
@@ -1,6 +1,20 @@
 
 package bccsp
 
+type ECDSAP256K1KeyGenOpts struct {
+	Temporary bool
+}
+
+func (opts *ECDSAP256K1KeyGenOpts) Algorithm() string {
+	return ECDSAP256
+}
+
+func (opts *ECDSAP256K1KeyGenOpts) Ephemeral() bool {
+	return opts.Temporary
+}
+
+
+
 type ECDSAP256KeyGenOpts struct {
 	Temporary bool
 }
