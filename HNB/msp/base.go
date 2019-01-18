061babb5e7dce6f8bc9900617c6dd8fc39cb2c0b
William.zhao <william.zhao@hnb.eco>
2019-01-19 02:23:34 +0800

    update msp base

--- HNB/msp/base.go
+++ HNB/msp/base.go
@@ -1,15 +1,18 @@
 package msp
 
 import (
-	"HNB/bccsp"
-	"HNB/bccsp/secp256k1"
-	"HNB/bccsp/sw"
-	"HNB/logging"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"encoding/json"
 	"fmt"
+	"github.com/HNB-ECO/HNB-Blockchain/HNB/bccsp"
+	"github.com/HNB-ECO/HNB-Blockchain/HNB/bccsp/secp256k1"
+	"github.com/HNB-ECO/HNB-Blockchain/HNB/bccsp/sw"
+	"github.com/HNB-ECO/HNB-Blockchain/HNB/cli/common"
+	"github.com/HNB-ECO/HNB-Blockchain/HNB/logging"
 	"io/ioutil"
+	"math/big"
+	"os"
 )
 
 type KeyPair struct {
@@ -50,6 +53,7 @@ func (kp *KeyPair) Init(path string) error {
 	case ECDSAP256:
 		key := new(ecdsa.PrivateKey)
 		key.Curve = secp256k1.S256()
+		key.D = new(big.Int)
 		key.D.SetBytes(sKeyPair.PriKey)
 		key.PublicKey.X, key.PublicKey.Y = elliptic.Unmarshal(secp256k1.S256(), sKeyPair.PubKey)
 		kp.Scheme = sKeyPair.Scheme
@@ -69,9 +73,41 @@ func Load(path string) (*SaveKeyPair, error) {
 		return nil, err
 	}
 	sKeyPair := new(SaveKeyPair)
-	err = json.Unmarshal(data, sKeyPair)
+	err = json.Unmarshal(data, &sKeyPair)
 	if err != nil {
 		return nil, err
 	}
 	return sKeyPair, nil
+}
+
+func Save(keypair *KeyPair, path string) error {
+	priKeyBytes, err := GetPriKeyBytes(keypair.Scheme, keypair.PriKey)
+	if err != nil {
+		return err
+	}
+
+	pubKeyBytes, err := GetPubKeyBytes(keypair.Scheme, keypair.PubKey)
+	if err != nil {
+		return err
+	}
+
+	saveKP := &SaveKeyPair{
+		Scheme: keypair.Scheme,
+		PubKey: pubKeyBytes,
+		PriKey: priKeyBytes,
+	}
+	data, err := json.Marshal(saveKP)
+	if err != nil {
+		return err
+	}
+	if common.FileExisted(path) {
+		filename := path + "~"
+		err := ioutil.WriteFile(filename, data, 0644)
+		if err != nil {
+			return err
+		}
+		return os.Rename(filename, path)
+	} else {
+		return ioutil.WriteFile(path, data, 0644)
+	}
 }
