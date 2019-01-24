1b84c9f965e7f036a6ebf8785921b803a8b221bc
William.zhao <william.zhao@hnb.eco>
2019-01-24 14:06:23 +0800

    update ledger cache

--- HNB/ledger/blockStore/cache.go
+++ HNB/ledger/blockStore/cache.go
@@ -1,22 +1,22 @@
 package blockStore
 
 import (
+	"github.com/HNB-ECO/HNB-Blockchain/HNB/ledger/blockStore/common"
+	"github.com/HNB-ECO/HNB-Blockchain/HNB/logging"
 	"github.com/bluele/gcache"
-	"HNB/ledger/blockStore/common"
-	"HNB/logging"
 )
 
 var BlockLog logging.LogModule
-const(
+
+const (
 	LOGTABLE_BLOCK string = "block"
 )
-
 
 type blockCache struct {
 	cache gcache.Cache
 }
 
-func NewBlockCache() *blockCache{
+func NewBlockCache() *blockCache {
 	bc := &blockCache{}
 	bc.cache = gcache.New(50).Build()
 	BlockLog.Infof(LOGTABLE_BLOCK, "create block cache size:%v", 50)
@@ -25,25 +25,25 @@ func NewBlockCache() *blockCache{
 
 //batch操作
 
-func(bc *blockCache) WriteBlock(block *common.Block) error{
+func (bc *blockCache) WriteBlock(block *common.Block) error {
 	BlockLog.Debugf(LOGTABLE_BLOCK, "write block cache blkNum:%v", block.BlockNum)
 	return bc.cache.Set(block.BlockNum, block)
 }
 
-func(bc *blockCache) RollbackBlock(blkNum uint64) error{
+func (bc *blockCache) RollbackBlock(blkNum uint64) error {
 	BlockLog.Debugf(LOGTABLE_BLOCK, "rollback block cache blkNum:%v", blkNum)
 	bc.cache.Remove(blkNum)
 	return nil
 }
 
-func(bc *blockCache) GetBlock(blkNum uint64) (*common.Block, error){
+func (bc *blockCache) GetBlock(blkNum uint64) (*common.Block, error) {
 	blk, err := bc.cache.Get(blkNum)
-	if err != nil && err != gcache.KeyNotFoundError{
+	if err != nil && err != gcache.KeyNotFoundError {
 		//BlockLog.Debugf(LOGTABLE_BLOCK, "blk cache err:%v", err.Error())
 		return nil, nil
 	}
 
-	if blk == nil{
+	if blk == nil {
 		return nil, nil
 	}
 
@@ -51,8 +51,3 @@ func(bc *blockCache) GetBlock(blkNum uint64) (*common.Block, error){
 
 	return blk.(*common.Block), nil
 }
-
-
-
-
-
