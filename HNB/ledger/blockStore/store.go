1b3bf13bd33cfe133dcf75730f2dabe80bc818a7
William.zhao <william.zhao@hnb.eco>
2019-01-24 14:06:55 +0800

    update ledger store

--- HNB/ledger/blockStore/store.go
+++ HNB/ledger/blockStore/store.go
@@ -1,46 +1,50 @@
 package blockStore
 
 import (
-	"HNB/ledger/blockStore/common"
-	dbComm "HNB/db/common"
 	"encoding/binary"
 	"encoding/json"
+	dbComm "github.com/HNB-ECO/HNB-Blockchain/HNB/db/common"
+	"github.com/HNB-ECO/HNB-Blockchain/HNB/ledger/blockStore/common"
+	"github.com/HNB-ECO/HNB-Blockchain/HNB/logging"
+	"github.com/HNB-ECO/HNB-Blockchain/HNB/msp"
 )
 
 type BlockStore struct {
 	cache *blockCache
-	db  dbComm.KVStore
+	db    dbComm.KVStore
 }
 
-func NewBlockStore(db dbComm.KVStore) common.BlockStore{
-	bc := &BlockStore{db:db}
+func NewBlockStore(db dbComm.KVStore) common.BlockStore {
+	BlockLog = logging.GetLogIns()
+	bc := &BlockStore{db: db}
 	bc.cache = NewBlockCache()
+	BlockLog.Info(LOGTABLE_BLOCK, "create block store")
 	return bc
 }
 
-func(bc *BlockStore) WriteBlock(block *common.Block) error{
-	blk, err := bc.cache.GetBlock(block.BlockNum)
-	if err != nil{
-		return err
-	}
-	if blk == nil{
-		bc.cache.WriteBlock(block)
-	}
-	b := make([]byte, 8)
-	binary.BigEndian.PutUint64(b, block.BlockNum)
+func (bc *BlockStore) WriteBlock(block *common.Block) error {
+	BlockLog.Infof(LOGTABLE_BLOCK, "write block num:%d", block.Header.BlockNum)
 
-	blkM,_ := json.Marshal(block)
+	bc.cache.WriteBlock(block)
+
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, block.Header.BlockNum)
+
+	blkM, _ := json.Marshal(block)
+
+	BlockLog.Debugf(LOGTABLE_BLOCK, "write db block num:%d", block.Header.BlockNum)
 
 	return bc.db.Put(b, blkM)
 }
 
-func(bc *BlockStore) RollbackBlock(blkNum uint64) error{
+func (bc *BlockStore) RollbackBlock(blkNum uint64) error {
+	BlockLog.Debugf(LOGTABLE_BLOCK, "del block num:%d", blkNum)
 	//优先检查缓存调用
 	blk, err := bc.cache.GetBlock(blkNum)
-	if err != nil{
+	if err != nil {
 		return err
 	}
-	if blk != nil{
+	if blk != nil {
 		bc.cache.RollbackBlock(blkNum)
 	}
 
@@ -50,30 +54,51 @@ func(bc *BlockStore) RollbackBlock(blkNum uint64) error{
 	bc.db.Delete(b)
 	return nil
 }
-
-func(bc *BlockStore) GetBlock(blkNum uint64) (*common.Block, error){
-	//优先检查缓存调用
-	blk, err := bc.cache.GetBlock(blkNum)
-	if err != nil{
+func (bc *BlockStore) GetBlockHash(blkNum uint64) ([]byte, error) {
+	blk, err := bc.GetBlock(blkNum)
+	if err != nil {
 		return nil, err
 	}
-	if blk != nil{
-		return nil, nil
+	//blkMarshal, err := json.Marshal(blk.Header)
+	//if err != nil{
+	//	return nil, err
+	//}
+	////TODO 计算Hash
+	//panic("calc hash")
+
+	//return blkMarshal, err
+	return bc.CalcBlockHash(blk)
+}
+func (bc *BlockStore) GetBlock(blkNum uint64) (*common.Block, error) {
+	//优先检查缓存调用
+	blk, err := bc.cache.GetBlock(blkNum)
+	if err != nil {
+		return nil, err
+	}
+	if blk != nil {
+		BlockLog.Debugf(LOGTABLE_BLOCK, "read block cache num:%d", blkNum)
+		return blk, nil
 	}
 
 	b := make([]byte, 8)
 	binary.BigEndian.PutUint64(b, blkNum)
 
+	BlockLog.Debugf(LOGTABLE_BLOCK, "read db block num:%d", blkNum)
 	blk1, err := bc.db.Get(b)
 
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 
 	blk2 := common.Block{}
-	json.Unmarshal(blk1, &blk2)
-
-	return &blk2, nil
+	err = json.Unmarshal(blk1, &blk2)
+	return &blk2, err
 }
 
-
+func (bc *BlockStore) CalcBlockHash(blk *common.Block) ([]byte, error) {
+	blkMarshal, err := json.Marshal(blk.Header)
+	if err != nil {
+		return nil, err
+	}
+	return msp.DoubleHash256(blkMarshal)
+}
